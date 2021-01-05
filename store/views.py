f72037a6553a9b723e02cf9aae6f0cfa818fa68f
feifeitang <jo05240625@gmail.com>
2021-01-05 12:16:30 +0000

    complete store order history and current order list

--- store/views.py
+++ store/views.py
@@ -1,32 +1,60 @@
 from django.shortcuts import render
 from .models import Order, OrderDetail, Inventory
+import datetime
 
 # Create your views here.
 
 
 def currentOrderRecord(request):
-    orderList = OrderDetail.objects.all().select_related('productOrderId', 'productInventoryId')
+    orderList = OrderDetail.objects.select_related(
+        'productOrderId', 'productInventoryId')
+    arr = []
+
+    for order in orderList:
+        now = datetime.datetime.now()
+
+        d = datetime.date(now.year, now.month, now.day)
+
+        if (d > order.productOrderId.deliveryDate) == False:
+            arr.append(order)
+
     return render(request, 'store/currentOrderRecord.html', {
-        'orderList': orderList
+        'orderList': arr
     })
 
 
 def hisotryOrderRecord(request):
-    return render(request, 'store/hisotryOrderRecord.html')
+    orderList = OrderDetail.objects.select_related(
+        'productOrderId', 'productInventoryId')
+    arr = []
+
+    for order in orderList:
+        now = datetime.datetime.now()
+
+        d = datetime.date(now.year, now.month, now.day)
+
+        if (d > order.productOrderId.deliveryDate) == True:
+            arr.append(order)
+    return render(request, 'store/hisotryOrderRecord.html', {
+        'historyList': arr
+    })
 
 
 def inventory(request):
     inventoryList = Inventory.objects.all()
 
-    return render(request, 'store/inventory.html',{
+    return render(request, 'store/inventory.html', {
         'inventoryList': inventoryList
     })
 
+
 def rmf(request):
-    return render(request, 'store/rmf.html',{})
+    return render(request, 'store/rmf.html', {})
+
 
 def value(request):
-    return render(request, 'store/value.html',{})
+    return render(request, 'store/value.html', {})
+
 
 def active(request):
-    return render(request, 'store/active.html',{})
+    return render(request, 'store/active.html', {})
