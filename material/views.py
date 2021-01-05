a6c8c07548da3bbcc3f8c4d54dac1368fe3cb6d4
feifeitang <jo05240625@gmail.com>
2021-01-05 12:35:50 +0000

    update html

--- material/views.py
+++ material/views.py
@@ -1,24 +1,53 @@
 from django.shortcuts import render
 from .models import Order, OrderDetails, Inventory
+import datetime
 
 # Create your views here.
 
 
 def currentOrderRecord(request):
-    orderList = OrderDetails.objects.all().select_related('materialOrderId', 'materialInventoryId')
-    return render(request, 'material/currentOrderRecord.html',{
-        'orderList': orderList
+    orderList = OrderDetails.objects.all().select_related(
+        'materialOrderId', 'materialInventoryId')
+    arr = []
+    for order in orderList:
+        now = datetime.datetime.now()
+
+        d = datetime.date(now.year, now.month, now.day)
+        print('d', d)
+        print('order.materialOrderId.purchaseDate',
+              order.materialOrderId.purchaseDate)
+        print(d > order.materialOrderId.purchaseDate)
+        if (d > order.materialOrderId.purchaseDate) == False:
+            arr.append(order)
+    return render(request, 'material/currentOrderRecord.html', {
+        'orderList': arr
     })
 
 
 def historyOrderRecord(request):
-    return render(request, 'material/hisotryOrderRecord.html')
+    orderList = OrderDetails.objects.all().select_related(
+        'materialOrderId', 'materialInventoryId')
+    arr = []
+    for order in orderList:
+        now = datetime.datetime.now()
+
+        d = datetime.date(now.year, now.month, now.day)
+
+        print('d', d)
+        print('order.materialOrderId.purchaseDate',
+              order.materialOrderId.purchaseDate)
+        print(d > order.materialOrderId.purchaseDate)
+        if (d > order.materialOrderId.purchaseDate) == True:
+            arr.append(order)
+    return render(request, 'material/hisotryOrderRecord.html', {
+        'historyList': arr
+    })
 
 
 def inventory(request):
     inventoryList = Inventory.objects.all()
 
-    return render(request, 'material/inventory.html',{
+    return render(request, 'material/inventory.html', {
         'inventoryList': inventoryList
     })
 
